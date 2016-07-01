89330bc79e6382876d7e3dbe1a730ac8bcac108e
fatihpolatli <fatihpolatli@gmail.com>
2016-07-01 14:33:21 +0300

    constants

--- src/main/resources/public/app/scripts/constants.js
+++ src/main/resources/public/app/scripts/constants.js
@@ -2,10 +2,10 @@ define(['app'], function(app) {
     'use strict';
 
 
-  //  var IP_ADDRESS = 'obscure-lake-10899.herokuapp.com';
+   var IP_ADDRESS = 'obscure-lake-10899.herokuapp.com';
   //  
-     var IP_ADDRESS = 'localhost:8080';
+    // var IP_ADDRESS = 'localhost:8080';
 
-    app.constant('API_END_POINT', 'http://' + IP_ADDRESS + '/restful');
+    app.constant('API_END_POINT', 'https://' + IP_ADDRESS + '/restful');
 
 });
